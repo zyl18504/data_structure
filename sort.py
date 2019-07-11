d38da85ccdc6448c73bc799dca09c7c447940376
Allen <shadowfly_zyl@hotmail.com>
2019-07-11 21:44:07 +0800

    add merge sort and quick sort

--- sort.py
+++ sort.py
@@ -64,7 +64,83 @@ def selection_sort(data):
     return data
 
 
+def merge_data(data, start, middle, end):
+    """
+    归并排序的合并算法
+    """
+    i, j = start, middle + 1
+    tmp = []
+    while i <= middle and j <= end:
+        # 判断一定要加等号才能保证排序的结果是稳定的
+        if data[i] <= data[j]:
+            tmp.append(data[i])
+            i += 1
+        else:
+            tmp.append(data[j])
+            j += 1
 
+    low = i if i <= middle else j
+    high = middle if i <= middle else end
+    a[low:high+1] = tmp
+
+
+def merge_sort_helper(data, start, end):
+    """
+    归并排序的递归方法
+    """
+    if start >= end:
+        return
+
+    middle = (start + end) / 2
+    merge_sort_helper(data, start, end)
+    merge_sort_helper(data, middle+1, end)
+    merge_data(data, start, middle, end)
+
+
+
+
+def merge_sort(data, n):
+    """
+    归并排序
+    n为数组data的长度
+    """
+    merge_sort_helper(data, 0, n-1)
+
+
+def quick_sort_partition(data, start, end):
+    """
+    快速排序获取分区点下标方法
+    """
+    high = data[end]
+    i = start
+    while start <= j <= end-1:
+        if data[j] < high:
+            data[i], data[j] = data[j], data[i]
+            i += 1
+
+    data[i], data[end] = data[end], data[i]
+    return i
+
+
+
+def quick_sort_helper(data, start, end):
+    """
+    快速排序递归方法
+    """
+    if start = end:
+        return
+
+    q = quick_sort_partition(data, start, end)
+    quick_sort_helper(data, start, q - 1)
+    quick_sort_helper(data, q + 1, end)
+
+
+
+def quick_sort(data, n):
+    """
+    快速排序
+    """
+    quick_sort_helper(data, 0, n-1)
 
 
 
